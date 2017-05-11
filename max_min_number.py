8862beabfec6ed2b37d3eb0b7a58f9d6580c4313
DomieBett <dbett49@gmail.com>
2017-05-11 10:46:02 +0300

    Added comments

--- max_min_number.py
+++ max_min_number.py
@@ -1,12 +1,21 @@
 def find_max_min(numbers):
+
+	#initial values for maximum and minimum numbers initialised to
+	#first item in a list.
 	max = numbers[0]
 	min = numbers[0]
+
+	#loops through the list and sets any number larger than the current
+	#value set as max to max less than the current min to min.
 
 	for i in range (1, len(numbers)):
 		if numbers[i] > max:
 			max = numbers[i]
 		elif numbers[i] < min:
 			min = numbers[i]
+
+	#if code completes while all numbers are the same, them list is of
+	#similar numbers. If not, return a list with max and min.
 
 	if min == max:
 		return [max]
